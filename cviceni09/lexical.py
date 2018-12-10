2803ab8f201c88310e52ba73e8cbee32e6f97bb1
malja <looorin@gmail.com>
2018-12-10 20:50:57 +0100

    Vloženo zadání

--- cviceni09/lexical.py
+++ cviceni09/lexical.py
@@ -1,15 +1,132 @@
+# Zadání:
+#########
+#
+# Proveďte lexikální analýzu Pythonovského programu, který může obsahovat pouze následující lexikální termy:
+#
+# - Str: Řetězec - začíná znaky
+#    
+#     ',",''',"""
+#
+# a může obsahovat sekvenci se znakem \. Pokud obsahuje řetězec sekvenci se znakem \X (X reprezentuje nějaký znak
+# z množiny \,',”,+,-), pak se do řetězce vloží pouze znak X.
+#
+# - Int: Celé číslo - integer:
+#
+#       integer        ::=  decimalinteger | octinteger | hexinteger | bininteger
+#       decimalinteger ::=  nonzerodigit digit* | "0"+
+#       nonzerodigit   ::=  "1"..."9"
+#       digit          ::=  "0"..."9"
+#       octinteger     ::=  "0" ("o" | "O") octdigit+
+#       hexinteger     ::=  "0" ("x" | "X") hexdigit+
+#       bininteger     ::=  "0" ("b" | "B") bindigit+
+#       octdigit       ::=  "0"..."7"
+#       hexdigit       ::=  digit | "a"..."f" | "A"..."F"
+#       bindigit       ::=  "0" | "1"
+#
+# - Rea: Reálné číslo - viz úloha 2
+# - Key: Klíčová slova Pythonu
+# - Var: Proměnné - začíná písmenem a pak může obsahovat písmena, čísla a znak _
+# - Ope: Operátory:
+#
+#       +       -       *       **      /       //      % 
+#       <<      >>      &       |       ^       ~
+#       <       >       <=      >=      ==      !=
+#
+# - Sep: Oddělovače:
+#
+#       (       )       [       ]       {       }
+#       ,       :       .       ;       @       =       ->
+#       +=      -=      *=      /=      //=     %=      @=
+#       &=      |=      ^=      >>=     <<=     **=
+#
+# Předpokládejte, že je program syntakticky správně. Komentáře neuvažujte, předpokládejte, že program je bez komentářů.
+# Progam lexical.py načte soubor zadaný jako vstupní parametr a vytiskne na výstup jednotlivé lexikální termy.
+#
+# Příklad
+# -------
+#
+# Vstup:
+"""
+def funkce(a,b):
+    c=''
+    a**=b
+    if a<b:
+        print('ahoj\'ky',a)
+    else:
+        print(0xff,0b11101,0o777,.90e-10,123E+5,c)
+        print('''To je dlouhy
+        retezec pres mnoho
+        radku''')
+funkce(-256+356,.85**.33)
+"""
+#
+# pak je výstupem Vašeho programu:
+#
+"""
+Key: def
+Var: funkce
+Sep: (
+Var: a
+Sep: ,
+Var: b
+Sep: )
+Sep: :
+Var: c
+Sep: =
+Str: 
+Var: a
+Sep: **=
+Var: b
+Key: if
+Var: a
+Ope: <
+Var: b
+Sep: :
+Var: print
+Sep: (
+Str: ahoj'ky
+Sep: ,
+Var: a
+Sep: )
+Key: else
+Sep: :
+Var: print
+Sep: (
+Int: 0xff
+Sep: ,
+Int: 0b11101
+Sep: ,
+Int: 0o777
+Sep: ,
+Rea: .90e-10
+Sep: ,
+Rea: 123E+5
+Sep: ,
+Var: c
+Sep: )
+Var: print
+Sep: (
+Str: To je dlouhy
+        retezec pres mnoho
+        radku
+Sep: )
+Var: funkce
+Sep: (
+Ope: -
+Int: 256
+Ope: +
+Int: 356
+Sep: ,
+Rea: .85
+Ope: **
+Rea: .33
+Sep: )
+"""
+#######################################################################################################################
+
 import sys
 import keyword as kw
 import tokenize
-
-strings = [
-    "'",
-    '"'
-]
-
-operators = [
-    "+", "-", "*", "**", "/", "//", "%", "<<", ">>", "&", "|", "^", "~", "<", ">", "<=", ">=", "==", "!="
-]
 
 separators = [
     "(", ")", "[", "]", "{", "}", ",", ":", ".", ";",
